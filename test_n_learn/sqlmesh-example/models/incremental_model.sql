498b7e3edec70f4bf5c152ade6393aa184279aab
mbyanfei <feiyan08@gmail.com>
2023-10-15 04:38:12 +0000

    testing sqlmesh CLI/UI - UI not working yet

---
+++ test_n_learn/sqlmesh-example/models/incremental_model.sql
@@ -0,0 +1,19 @@
+MODEL (
+    name sqlmesh_example.incremental_model,
+    kind INCREMENTAL_BY_TIME_RANGE (
+        time_column ds
+    ),
+    start '2020-01-01',
+    cron '@daily',
+    grain [id, ds]
+);
+
+SELECT
+    id,
+    item_id,
+    'z' as new_column, -- added column
+    ds,
+FROM
+    sqlmesh_example.seed_model
+WHERE
+    ds between @start_ds and @end_ds
